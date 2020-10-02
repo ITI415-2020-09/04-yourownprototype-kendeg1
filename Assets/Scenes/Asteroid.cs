0f0535e9dd347614b1f548fadf2ccc1f9a64fc8e
kendeg1 <kendeg1@gmail.com>
2020-10-01 22:22:05 -0400

    asteroid script
    working on spawning asteroids, similar to clouds from mission demolition

---
+++ Assets/Scenes/Asteroid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    Transform asteroidT;
+    Vector3 randomRotation;
+
+    void awake()
+    {
+        asteroidT = transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //random size
+        Vector3 scale = Vector3.one;
+        scale.x = randomRotation.Range(.8f, 1.2f);
+        asteroidT.localScale = scale;
+        //random rotation
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
